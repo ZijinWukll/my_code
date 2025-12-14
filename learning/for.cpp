2fa5afcf666da7d208b0c59b714ada534677a560
ZijinWukll <keliaoliao@qq.com>
2025-12-14 22:33:14 +0800

    0

--- learning/for.cpp
+++ learning/for.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 int main()
 {
-    int sum = 0;
-    int a;
-    cin >> a;
-    for (int i = 0; i < a; i++)
+    while (1)
     {
-        sum += 1;
+        cout << "1";
     }
-    cout << sum << endl;
     return 0;
 }
