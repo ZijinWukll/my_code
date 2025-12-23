5ebad05428c339ae8155eaba8125029d30c18be4
ZijinWukll <keliaoliao@qq.com>
2025-12-23 20:21:23 +0800

    0

--- learning/Lean/mylean/lakefile.lean
+++ learning/Lean/mylean/lakefile.lean
@@ -1,7 +1,11 @@
 import Lake
 open Lake DSL
 
+
 package mylean where
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4" @ "master"
 
 lean_exe myleanExe {
   root := `Main
--- learning/Lean/mylean/src/hello.lean
+++ learning/Lean/mylean/src/hello.lean
@@ -1,5 +1,2 @@
-/- Lean 入门示例：输出 Hello, Lean! -/
-/- main 函数是 Lean 程序的入口 -/
-def main : IO Unit := do
-  -- 输出一行欢迎信息
-  IO.println "Hello, Lean!"
+import mathlib
+example (x y : ℕ) (h : y = x + 7) : 2 * y = 2 * (x + 7) := by
